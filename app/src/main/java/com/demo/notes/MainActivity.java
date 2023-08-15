f363ed03ee84f54a81216c8566e12e10a626d806
SolomonVP <Lum7023@yandex.ru>
2023-08-15 22:09:04 +0300

    ref +SQLite

--- app/src/main/java/com/demo/notes/MainActivity.java
+++ app/src/main/java/com/demo/notes/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaRouter;
 import android.os.Bundle;
 import android.view.View;
@@ -21,23 +24,47 @@ public class MainActivity extends AppCompatActivity {
     public static final ArrayList<Note> notes = new ArrayList<>();
     private NotesAdapter adapter;
 
+    private NotesDBHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
-        if (notes.isEmpty()) {
-            notes.add(new Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
-            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
-            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
-            notes.add(new Note("Стоматолог", "Вылезить зубы", "Понедельник", 2));
-            notes.add(new Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1));
-            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
-            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+        dbHelper = new NotesDBHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+//        if (notes.isEmpty()) {
+//            notes.add(new Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
+//            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+//            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+//            notes.add(new Note("Стоматолог", "Вылечить зубы", "Понедельник", 2));
+//            notes.add(new Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1));
+//            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+//            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+//        }
+//
+//        for (Note note : notes) {
+//            ContentValues contentValues = new ContentValues();
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriority());
+//            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+//        }
 
+        ArrayList<Note> notesFromDB = new ArrayList<>();
+        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
+            String day_of_week = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
+            int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
+            Note note = new Note(title, description, day_of_week, priority);
+            notesFromDB.add(note);
         }
+        cursor.close();
 
-        adapter = new NotesAdapter(notes);
+        adapter = new NotesAdapter(notesFromDB);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
         adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
